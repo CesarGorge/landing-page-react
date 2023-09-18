02820b5c830273602336ebe4132b40cdb5bba8a0
CesarGorge <cesargorgeee@gmail.com>
2023-09-18 07:18:12 +0000

    landing page

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Footer = (props) => {
+  return (
+    <footer className="footer mt-auto bg-dark py-3">
+      <div className="container-footer">
+        <span className="text-light text-center d-flex justify-content-center align-items-center">
+          Copyright <i className="fa-solid fa-copyright text-light mx-2"></i>{" "}
+          Your Website 2019
+        </span>
+      </div>
+    </footer>
+  );
+};
